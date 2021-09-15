2ce013957ee8680176b44720d8ef19826b10ff9f
Przemek <przem.nowosada@gmail.com>
2021-09-15 18:48:33 +0200

    Cart item functionality

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -6,11 +6,11 @@ export class Product {
     price: number;
     imageUrl: string;
 
-    constructor(id, name, description = '', price = 0, imageUrl = 'https://duckduckgo.com/?q=man+shoe&t=brave&iar=images&iax=images&ia=images&iai=https%3A%2F%2Fwww.shoesinternational.co.uk%2Fmedia%2Fcatalog%2Fproduct%2Fe%2Fc%2Fecco__430004-01283__main.jpg') {
-      this.id = id
-      this.name = name
-      this.description = description
-      this.price = price
-      this.imageUrl = imageUrl
-    }
+    constructor(id, name, description = '', price = 0, imageUrl = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.jKrT1HwDViFQVBwfKsMrkAHaFF%26pid%3DApi&f=1') {
+        this.id = id
+        this.name = name
+        this.description = description
+        this.price = price
+        this.imageUrl = imageUrl
+      }
   }
