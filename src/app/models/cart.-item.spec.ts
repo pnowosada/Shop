bf54e9d4ddd0578ea8469d6aa4932f067daee998
Przemek <przem.nowosada@gmail.com>
2021-09-23 21:08:28 +0200

    API Integration for Cart

---
+++ src/app/models/cart.-item.spec.ts
@@ -0,0 +1,8 @@
+import { CartService } from '../services/cart.service';
+import { CartItem} from'./cart-item'
+
+describe('CartItem', () => {
+  it('should create an instance', () => {
+   //expect(new CartItem()).toBeTruthy();
+  });
+});
