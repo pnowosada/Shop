34186cb362be771bdd7fb9eac5f221d512a38304
Przemek <przem.nowosada@gmail.com>
2021-10-02 15:34:54 +0200

    Added modules and router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
+const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch:'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'shop', component: ShoppingCartComponent},
+  //'**' math every routh prefix and sufix
+  {path: '**',component:PageNotFoundComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+
+ }
