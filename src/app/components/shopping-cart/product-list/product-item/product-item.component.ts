1a800eef75bcaa58d9facc8cc9a9144571de775e
Przemek <przem.nowosada@gmail.com>
2021-10-02 13:11:40 +0200

    Added Add to wishlist button

--- src/app/components/shopping-cart/product-list/product-item/product-item.component.ts
+++ src/app/components/shopping-cart/product-list/product-item/product-item.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit , Input} from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { MessengerService } from 'src/app/services/messenger.service';
 import { CartService } from 'src/app/services/cart.service';
+import { WishlistService } from 'src/app/services/wishlist.service';
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
@@ -9,17 +10,34 @@ import { CartService } from 'src/app/services/cart.service';
 })
 export class ProductItemComponent implements OnInit {
 
-  @Input() productItem: Product
-  constructor(
-    private msg:MessengerService,
-    private cartService:CartService
-    ) { }
+  @Input() productItem: Product;
 
-  ngOnInit(){
+  @Input() addedToWishlist: boolean;
+
+  constructor(
+    private msg: MessengerService,
+    private cartService: CartService,
+    private wishlistService: WishlistService
+  ) { }
+
+  ngOnInit() {
   }
-handleAddToCart(){
-  this.cartService.addProductToCart(this.productItem).subscribe(()=>{
-    this.msg.sendMsg(this.productItem)
-  })
-}
+
+  handleAddToCart() {
+    this.cartService.addProductToCart(this.productItem).subscribe(() => {
+      this.msg.sendMsg(this.productItem)
+    })
+  }
+
+  handleAddToWishlist() {
+    this.wishlistService.addToWishlist(this.productItem.id).subscribe(() => {
+      this.addedToWishlist = true;
+    })
+  }
+
+  handleRemoveFromWishlist() {
+    this.wishlistService.removeFromWishlist(this.productItem.id).subscribe(() => {
+      this.addedToWishlist = false;
+    })
+  }
 }
--- src/app/components/shopping-cart/product-list/product-list.component.ts
+++ src/app/components/shopping-cart/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
+import { WishlistService } from 'src/app/services/wishlist.service';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -9,12 +11,27 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
 
   productList: Product[]=[]
-  constructor(private productService: ProductService) { }
+  wishlist:any[]=[]
+  constructor(
+    private productService: ProductService,
+    private wishlistService: WishlistService
+    ) { }
 
   ngOnInit(){
-   this.productService.getProduct().subscribe((products)=>{
-     this.productList=products;
-   })
+    this.loadProducts();
+    this.loadWishlist();
+   }
+   loadProducts(){
+    this.productService.getProduct().subscribe((products)=>{
+      this.productList=products;
+  })
   }
+  loadWishlist(){
+    this.wishlistService.getWishList().subscribe(productIds=>{
+      console.log(productIds)
+      this.wishlist=productIds
+    })
+  }
+
 
 }
